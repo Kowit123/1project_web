05abfc5219d6ad88c4bf1ddefc765324a3e64591
Kowit An-chean <kowitanchean238@gmail.com>
2024-12-20 14:59:49 +0700

    add navbar

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function toggleHamburgerIcon(toggleHamburger) {
   toggleHamburger.classList.toggle("change");
 }
+
+
+
+let hamburgericon = document.getElementById("hamburgericon");
+
+if (hamburgericon){
+  hamburgericon.addEventListener("click", function() {
+    let mobilenav = document.getElementById("mobilenav");
+    if(mobilenav){
+      if(mobilenav.style.display === "block"){
+        mobilenav.style.display = "none"
+      }
+      else{ 
+        mobilenav.style.display = "block"
+      }
+    }
+  })
+}
