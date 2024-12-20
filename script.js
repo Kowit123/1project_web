f6fc3ea7fcb7ac6e5efbfb4f7ecd593f38937270
Kowit An-chean <kowitanchean238@gmail.com>
2024-12-20 15:39:44 +0700

    1

--- script.js
+++ script.js
@@ -17,5 +17,24 @@ if (hamburgericon){
         mobilenav.style.display = "block"
       }
     }
+
   })
 }
+
+
+
+function handleResize() {
+  const width = window.innerWidth;
+
+  if (width > 672) {
+    mobilenav.style.display = 'none'; // ธีมสำหรับหน้าจอเล็ก
+  }
+}
+
+window.addEventListener('resize', handleResize);
+
+// เรียกฟังก์ชันครั้งแรกเพื่อกำหนดธีมตอนโหลด
+handleResize();
+
+
+
